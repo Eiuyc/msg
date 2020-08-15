82754d337472167c8384e56513d94e8279741880
Eiuyc <408631569@qq.com>
2020-08-15 17:23:38 +0000

    msg

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,2 @@
 var msg = document.getElementById('msg')
-msg.innerHTML = tnrrmnyrgqsivmqivgjvijxprixzuuijutuijpjminrmyrvtvhgr
+msg.innerHTML = "tqrvnhmrgqsivmqivgjvijxprixzuuijutuijpjminrmyytnrqhr"

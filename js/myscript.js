233a60b4a1a8aaebe914a2d25fa95b0360df86f6
Eiuyc <408631569@qq.com>
2020-08-15 17:26:48 +0000

    msg

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,2 @@
 var msg = document.getElementById('msg')
-msg.innerHTML = "tqrvnhmrgqsivmqivgjvijxprixzuuijutuijpjminrmyytnrqhr"
+msg.innerHTML = "tyqmwmhrgqsivmqivgjvijxprixzuuijutuijpjminrmywhqyvym"

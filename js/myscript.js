57ed0051ee34525ea10a9cd4194623c68c779565
Eiuyc <408631569@qq.com>
2020-08-19 10:51:01 +0000

    msg

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,2 @@
 var msg = document.getElementById('msg');
-msg.innerHTML = "nrnyttqrgqsivmqivgjyignswiwnxuiuuhtivntmijhmygwqqtty";
+msg.innerHTML = "hqnnrrvrgqsivmqivgjyignswijmhhiysswispngiwpspvmtyhqr";
